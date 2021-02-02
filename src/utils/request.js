e54e7221abafd29ccfb3c09f8a2d28b86e76cfe0
bykoala <l>
2021-02-02 18:04:09 +0800

    更新列表页编码实现

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import {Redirect} from "react-router-dom";
+
+
+
 
 export function postRequest(urlApi,data) {
    axios({
@@ -9,9 +13,39 @@ export function postRequest(urlApi,data) {
        headers:{"Content-Type":"application/x-www-form-urlencoded",
        'Accept':'application/json'}
    }).then(function(res){
-       if(res.code==10000){
-           
+       if(res.data.code==10000){
+        console.log("发布成功了");
+        alert("发布成功了")
+        // <Redirect to="/404"/>
+       }else if(res.data.code=10001){
+           console.log("发布失败");
+ 
+       }else{
+           console.log("发布异常")
        }
-       console.log(res)
+       console.log("res",res)
    })
+}
+
+
+export function getRequest(url,callback)  {
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(json => {
+            callback(json)
+            if (json.code == 10000) {
+
+                console.log("请求成功");
+            } else if (json.code == 10001) {
+                console.log("请求失败")
+            }
+        })
+        .catch(err => {
+            console.log('异常！！！', err);
+        })
 }
--- src/views/Updatelist.js
+++ src/views/Updatelist.js
@@ -1,50 +1,88 @@
-import React, {useState, useEffect } from 'react';
-import { Card,Descriptions, Divider  } from "antd";
-
-class Updatelist extends React.Component{
+import { Card, Descriptions, Divider, version } from "antd";
+import { getRequest } from "../utils/request";
+import React from 'react';
+class ShowUpdateComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {comps : []}
-}
-  render(){
-    let desc = (
-      <Descriptions title="更新列表" column="2">
-      <Descriptions.Item label="版本号">Zhou Maomao</Descriptions.Item>
-      <Descriptions.Item label="文件大小">1810000000</Descriptions.Item>
-      <Descriptions.Item label="更新日期">Hangzhou, Zhejiang</Descriptions.Item>
-      <Descriptions.Item label="MD5值">
-        No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-      </Descriptions.Item>
-      <Descriptions.Item label="下载">empty</Descriptions.Item>
-      <Descriptions.Item label="更新内容">empty</Descriptions.Item>
-    </Descriptions>
-    );
-    const MainComponent = () => <div>
-      {desc}
-      <Divider></Divider>
-    </div> 
-    const {comps } = this.state;
-    const testMap = {type:"Fiat", model:500, color:"white"};
-    const test = ()=>{
-      for(let i=0;i<Object.keys(testMap).length;i++){
-        console.log("aaaaaaaaaaaaa")
-        this.setState({comps: comps.concat([Date.now()])})
-      }
-    }
-        return (
-          <Card
-          style={{
-            padding: "10px",
-            width: 1000,
-          }}
-        >
-           {comps.map(comp => {
-                    return <MainComponent key={comp} />
-                })}
-            <button onClick={test}>加组件</button>
-         
-        </Card> 
-  )
   }
+  componentShouldUpdate(){
+    console.log("showupdate===========",this.props)
+  }
+  render() {
+    return (
+      <Descriptions title="更新列表" column="2">
+        <Descriptions.Item label="版本号">{this.props.version}</Descriptions.Item>
+        <Descriptions.Item label="文件大小">{this.props.fileSize}</Descriptions.Item>
+        <Descriptions.Item label="更新日期">{this.props.creatTime}</Descriptions.Item>
+        <Descriptions.Item label="MD5值">{this.props.md5}
+        </Descriptions.Item>
+        <Descriptions.Item label="下载">{this.props.fileUrl}</Descriptions.Item>
+        <Descriptions.Item label="更新内容">{this.props.content}</Descriptions.Item>
+      </Descriptions>
+    )
+  }
+}
+
+class Updatelist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      comps: [],
+      data:{}
+     }
+    //  this.printData=this.printData.bind(this)
+  }
+  componentWillMount() {
+    getRequest("http://localhost:8088/getUpdateHistory", this.receiveData)
+  }
+  receiveData=(data)=> {
+    // this.state.data=data
+    this.setState({
+      data: data
+    });
+
+    const loopGetData = this.state.data.data;
+    const test = () => {
+      let compsbox = [];
+      // for(let i=0;i<Object.keys(testMap).length;i++){ }
+      for (let v in loopGetData) {
+        <ShowUpdateComponent 
+        version={loopGetData[v].Version} fileSize="tes文件大小t" 
+        creatTime="tes更新日期tt" md5="tesMD5值t" 
+        fileUrl="te下载tt" content="t更新内容tt">
+        </ShowUpdateComponent>
+        // 譬如：我现在取到version的数据：testMap[v].Version
+        compsbox = compsbox.concat([Date.now()])
+      }
+      this.setState({
+        comps: compsbox
+      });
+    }
+    test()
+  }
+ 
+  render() {
+   
+    const MainComponent = () => <div>
+      <ShowUpdateComponent/>
+      <Divider></Divider>
+    </div>
+    const { comps } = this.state;
+    return (
+      <Card
+        style={{
+          padding: "10px",
+          width: 1000,
+        }}
+      >
+        {comps.map(comp => {
+          return <MainComponent key={comp} />
+        })}
+        {/* <button onClick={test}>加组件</button> */}
+      </Card>
+    )
+  }
+
+ 
 }
 export default Updatelist
